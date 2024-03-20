2f716a2e4f7ad50ce7bb0dfc88510a25100f917a
Amal-S-2003 <amalshibu379@gmail.com>
2024-03-20 12:22:47 +0530

    creted models

--- customers/models.py
+++ customers/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Model for product
-class Product(models.Model):
+# Customer model
+from django.contrib.auth.models import User
+class Customer(models.Model):
     LIVE=1
     DELETE=0
     DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
-    title=models.CharField(max_length=200)
-    price=models.FloatField()
-    description=models.TextField()
-    image=models.ImageField(upload_to='media/')
-    priority=models.IntegerField(default=0)
+    name=models.CharField(max_length=200)
+    address=models.TextField()
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
+    phone=models.CharField(max_length=10)
     delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
